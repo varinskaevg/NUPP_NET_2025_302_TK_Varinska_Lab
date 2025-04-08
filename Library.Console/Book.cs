954afdd211bd42dfc38199ff1efbe1ab2348f19f
Evgenia <128784367+varinskaevg@users.noreply.github.com>
2025-04-08 23:00:10 +0300

    lab1

---
+++ Library.Console/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Console
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Book other)
+            {
+                return Title == other.Title && Author == other.Author;
+            }
+            return false;
+        }
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.GetHashCode() ^ Author.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}";
+        }
+    }
+}
---
+++ Library.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using LibraryManagementSystem.Common;
+using Book = Library.Console.Book;
+
+namespace Library.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICrudService<Book> bookService = new CrudService<Book>();
+
+            var book1 = new Book("1984", "George Orwell", 1949);
+            var book2 = new Book("Brave New World", "Aldous Huxley", 1932);
+
+            bookService.Create(book1);
+            bookService.Create(book2);
+
+            System.Console.WriteLine("=== Книги після додавання ===");
+            foreach (var book in bookService.ReadAll())
+                System.Console.WriteLine(book);
+
+            var updatedBook = new Book("1984", "George Orwell (Updated)", 1949);
+            bookService.Update(updatedBook);
+
+            System.Console.WriteLine("\n=== Книги після оновлення ===");
+            foreach (var book in bookService.ReadAll())
+                System.Console.WriteLine(book);
+
+            bookService.Remove(book2);
+
+            System.Console.WriteLine("\n=== Книги після видалення ===");
+            foreach (var book in bookService.ReadAll())
+                System.Console.WriteLine(book);
+
+            bookService.Save("books.json");
+
+            bookService = new CrudService<Book>();
+            bookService.Load("books.json");
+
+            System.Console.WriteLine("\n=== Книги після завантаження з файлу ===");
+            foreach (var book in bookService.ReadAll())
+                System.Console.WriteLine(book);
+
+            System.Console.ReadKey();
+        }
+    }
+}
